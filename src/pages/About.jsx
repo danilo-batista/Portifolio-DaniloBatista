75ba0ebf2e5e04567b20ed9d8ae9453b02047963
Danilo Batista <60406813+danilo-batista@users.noreply.github.com>
2025-06-25 15:09:36 -0300

    feat: create an 'Experience' component.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
 import { HeroAbout } from '../components/Hero/HeroAbout';
+import { Experience } from '../components/Experience/Experience';
 
 export function About() {
   return (
     <>
       <HeroAbout />
+      <Experience />
     </>
   );
 }
