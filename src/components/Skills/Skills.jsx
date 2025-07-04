8c595752ec5fbec55aee432d4419860f67619bd6
Danilo Batista <60406813+danilo-batista@users.noreply.github.com>
2025-07-04 18:57:56 -0300

    feat: add 'Skills' component.

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,22 @@
+import styles from '../Skills/Skills.module.scss';
+
+export function Skills() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.container__title}>Minhas Habilidades</div>
+
+        <ul className={styles.container__listItems}>
+          <li className={styles.container__items}>React.js</li>
+          <li className={styles.container__items}>JavaScript</li>
+          <li className={styles.container__items}>HTML | CSS</li>
+          <li className={styles.container__items}>Git | GitHub</li>
+          <li className={styles.container__items}>Trabalho em equipe</li>
+          <li className={styles.container__items}>Resolução de problemas</li>
+          <li className={styles.container__items}>Proatividade</li>
+          <li className={styles.container__items}>Melhoria contínua</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import { HeroAbout } from '../components/Hero/HeroAbout';
 import { Experience } from '../components/Experience/Experience';
+import { Skills } from '../components/Skills/Skills';
 
 export function About() {
   return (
     <>
       <HeroAbout />
+      <Skills />
       <Experience />
     </>
   );
