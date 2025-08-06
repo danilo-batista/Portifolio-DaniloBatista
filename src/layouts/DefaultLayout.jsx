97aa2a5fae56ca1d641818cb8bfe07d10fb5ba2b
Danilo Batista <60406813+danilo-batista@users.noreply.github.com>
2025-08-06 13:00:31 -0300

    fix: update base files for sass to improve organization.

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { Footer } from '../components/Footer/Footer';
 import { Header } from '../components/Header/Header';
-
 import styles from '../layouts/DefaultLayout.module.scss';
 
 export function DefaultLayout() {
