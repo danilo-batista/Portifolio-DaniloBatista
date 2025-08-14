c79bae3deb9c8303453cc4897b6d0e143c6475ac
Danilo Batista <60406813+danilo-batista@users.noreply.github.com>
2025-08-13 22:34:17 -0300

    fix: update heroAbout with new css.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,12 @@
 import { CareerList } from '../components/Career/CareerList';
-import { HeroAbout } from '../components/Hero/HeroAbout';
+import { NewHeroAbout } from '../components/Hero/NewHeroAbout';
 import { KnowledgeList } from '../components/Knowledge/KnowledgeList';
 import { Skills } from '../components/Skills/Skills';
 
 export function About() {
   return (
     <>
-      <HeroAbout />
+      <NewHeroAbout />
       <Skills />
       <CareerList />
       <KnowledgeList />
