4bc73773c794bfe37565189457fd6c874ce63824
Danilo Batista <60406813+danilo-batista@users.noreply.github.com>
2025-08-28 11:36:00 -0300

    fix: add some config for vercel and vite deploy.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist"
+  },
+  server: {
+    historyApiFallback: true
+  }
 });
